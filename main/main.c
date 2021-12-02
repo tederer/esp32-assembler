209aae7a03005502a1ded7ea5be269bec26e9d09
Thomas Ederer <thomas@neusiedl.coderdojo.net>
2021-12-02 18:54:44 +0100

    improved usability

--- main/main.c
+++ main/main.c
@@ -26,15 +26,11 @@
 #define ULP_PROGRAM_COMMAND_SIZE_IN_BYTES       4
 #define ULP_PROGRAM_HALT_COMMANDS_COUNT         2
 
-static const char* TAG = "main";
-
 static uint8_t ulpProgram[ULP_PROGRAM_HEADER_SIZE_IN_BYTES + (ULP_PROGRAM_MAX_COMMAND_COUNT + ULP_PROGRAM_HALT_COMMANDS_COUNT) * ULP_PROGRAM_COMMAND_SIZE_IN_BYTES];
 // The reg_wr command disables the ULP timer to ensure that the ULP program gets executed only once (see technical reference manual "29.5 ULP Program Execution").
 static char *HALT_COMMANDS[ULP_PROGRAM_HALT_COMMANDS_COUNT] = { "reg_wr 6, 24, 24, 0", "halt"};
 static size_t nextCommandIndex = 0;
-static bool userWantsToStartUlpProgram = false;
-
-extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
+static bool userEnteredNewCommands = false;
 
 static void appendHaltCommandsToUlpProgram(const uint8_t *program);
 static void loadUlpProgram(const uint8_t *program);
@@ -42,13 +38,15 @@ static void startUlpProgram(size_t indexOfFirstCommand);
 static void initSerialInterface();
 static void handleCommands(void *parameters);
 static void processNextLine(const uint8_t *line);
+static void printCommands(const uint8_t *firstByteOfFirstCommand, size_t commandCount);
 static void printUlpProgram(const uint8_t *programStart);
-static void printRtcSlowMemory(size_t commandsToPrint);
+static void printRtcSlowMemory();
 static void initializeUlpProgram();
 static void setBytesInUlpProgram(size_t commandIndex, CommandBytes *commandBytes);
 static void createVariable(const char *command);
 static void createCommand(const char *command);
-static void runProgram(const char *command);
+static bool runProgram(const char *command);
+static void printHelp();
 static bool regexMatches(const char *text, const char *pattern);
 
 // ULP program binary according to https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/ulp.html?highlight=ulp%20magic#_CPPv415ulp_load_binary8uint32_tPK7uint8_t6size_t
@@ -73,7 +71,7 @@ void app_main()
 }
 
 static void initializeUlpProgram() {
-   ESP_LOGI(TAG, "initializing ULP program");
+   printf("Initializing ULP program ...\n");
    struct UlpBinary* metaData = (struct UlpBinary*)ulpProgram;
    metaData->magic       = 0x00706c75;
    metaData->textOffset  = 12;
@@ -81,21 +79,23 @@ static void initializeUlpProgram() {
    metaData->dataSize    = 0;
    metaData->bssSize     = 0;
 
-   // set all commands to nop
    Result noopCommand = getCommandBytesFor((uint8_t*)"nop");
    
    for(size_t commandIndex = 0; commandIndex < ULP_PROGRAM_MAX_COMMAND_COUNT; commandIndex++) {
       setBytesInUlpProgram(commandIndex, &(noopCommand.commandBytes));
    }
+
+   nextCommandIndex = 0; 
+   userEnteredNewCommands = false;     
 }
 
 static void appendHaltCommandsToUlpProgram(const uint8_t *program) {
    struct UlpBinary* metaData = (struct UlpBinary*)program;
-   metaData->magic       = 0x00706c75;
-   metaData->textOffset  = 12;
-   metaData->textSize    = (nextCommandIndex + ULP_PROGRAM_HALT_COMMANDS_COUNT) * ULP_PROGRAM_COMMAND_SIZE_IN_BYTES;
-   metaData->dataSize    = 0;
-   metaData->bssSize     = 0;
+   metaData->magic      = 0x00706c75;
+   metaData->textOffset = 12;
+   metaData->textSize   = (nextCommandIndex + ULP_PROGRAM_HALT_COMMANDS_COUNT) * ULP_PROGRAM_COMMAND_SIZE_IN_BYTES;
+   metaData->dataSize   = 0;
+   metaData->bssSize    = 0;
 
    size_t commandIndexOfFirstHaltCommand = nextCommandIndex;
 
@@ -106,7 +106,7 @@ static void appendHaltCommandsToUlpProgram(const uint8_t *program) {
 }
 
 static void loadUlpProgram(const uint8_t *program) {
-   ESP_LOGI(TAG, "loading ULP program into ULP memory ...");
+   printf("Loading your program into RTC memory ...\n");
    struct UlpBinary* metaData = (struct UlpBinary*)program;
    uint32_t programSizeInBytes = ULP_PROGRAM_HEADER_SIZE_IN_BYTES + metaData->textSize + metaData->dataSize + metaData->bssSize;
    ESP_ERROR_CHECK(ulp_load_binary(0, program, programSizeInBytes / sizeof(uint32_t)));
@@ -114,7 +114,7 @@ static void loadUlpProgram(const uint8_t *program) {
 
 static void startUlpProgram(size_t indexOfFirstCommand)
 {
-   ESP_LOGI(TAG, "starting ULP program (indexOfFirstCommand=%d)", indexOfFirstCommand);
+   printf("Starting at command index %d.\n", indexOfFirstCommand);
    ESP_ERROR_CHECK(ulp_run(indexOfFirstCommand));
 }
 
@@ -141,11 +141,9 @@ static void handleCommands(void *parameters) {
    size_t insertationPosition = 0;
    
    vTaskDelay(100 / portTICK_PERIOD_MS);
-
    initSerialInterface();
    
-   ESP_LOGI(TAG, "starting input reading loop");
-   while (!userWantsToStartUlpProgram) {
+   while (true) {
       readBytes = uart_read_bytes(SERIAL_PORT, buffer, 1, 1000 / portTICK_PERIOD_MS);
       if (readBytes > 0) {
          if (buffer[0] != LF) {
@@ -158,14 +156,22 @@ static void handleCommands(void *parameters) {
 
          if (insertationPosition == maxLineLength) {
             line[maxLineLength] = 0;
-            printf("ERROR: max line length (%d) reached -> ignoring \"%s\"\n", maxLineLength, line);
+            printf("ERROR: Maximum line length (%d) reached -> ignoring \"%s\".\n", maxLineLength, line);
             insertationPosition = 0;
          }
       }
    }
-
-   ESP_LOGI(TAG, "finished input reading loop");
+   
    vTaskDelete(NULL);
+}
+
+static void printHelp() {
+   printf("\nIn addition to the ULP instructions (see https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/ulp_instruction_set.html), the following commands are supported:\n\n");
+   printf("var(<value>)                stores <value> at the current command index\n");
+   printf("run <indexOfFirstCommand>   executes your program and displays the memory used by it\n");
+   printf("list                        displays the memory used by your program\n");
+   printf("reset                       removes all alreay entered commands\n\n");
+   printf("For further details visit https://github.com/tederer/esp32-assembler.\n\n");
 }
 
 static void processNextLine(const uint8_t *line) {
@@ -175,12 +181,17 @@ static void processNextLine(const uint8_t *line) {
    char *trimmedLineInLowerCase = (char*)toLowerCase(trim(copyOfLine));
 
    if (regexMatches(trimmedLineInLowerCase, "run [0-9]+")) {
-      runProgram(trimmedLineInLowerCase);
-      vTaskDelay(500 / portTICK_PERIOD_MS);
-      printRtcSlowMemory(nextCommandIndex + ULP_PROGRAM_HALT_COMMANDS_COUNT);
+      if (runProgram(trimmedLineInLowerCase)) {
+         userEnteredNewCommands = false; 
+         vTaskDelay(500 / portTICK_PERIOD_MS);
+         printRtcSlowMemory();
+      }
+   } else if (strcmp(trimmedLineInLowerCase, "list") == 0) {
+      printRtcSlowMemory();  
    } else if (strcmp(trimmedLineInLowerCase, "reset") == 0) {
       initializeUlpProgram();
-      nextCommandIndex=0;      
+   } else if ((strcmp(trimmedLineInLowerCase, "help") == 0) || (strlen(trimmedLineInLowerCase) == 0)) {
+      printHelp(); 
    } else if (regexMatches(trimmedLineInLowerCase, "var\\([0-9]+\\)")) {
       createVariable(trimmedLineInLowerCase);
    } else {
@@ -197,34 +208,44 @@ static void setBytesInUlpProgram(size_t commandIndex, CommandBytes *commandBytes
    ulpProgram[indexOfFirstByte + 3] = commandBytes->byte3;
 }
 
-static void printUlpProgram(const uint8_t *programStart) {
-   struct UlpBinary *ulpBinary = (struct UlpBinary*)programStart;
-   ESP_LOGI(TAG, "magic         = %d", ulpBinary->magic);
-   ESP_LOGI(TAG, "textOffset    = %d", ulpBinary->textOffset);
-   ESP_LOGI(TAG, "textSize      = %d", ulpBinary->textSize);
-   ESP_LOGI(TAG, "dataSize      = %d", ulpBinary->dataSize);
-   ESP_LOGI(TAG, "bssSize       = %d", ulpBinary->bssSize);
-   
+static void printCommands(const uint8_t *firstByteOfFirstCommand, size_t commandCount) {
    char command[50];
-
-   const uint8_t *codeStart = programStart + ulpBinary->textOffset;
-
-   ESP_LOGI(TAG, "             byte3  byte2  byte1  byte0");
-   for (size_t offset = 0; offset < ulpBinary->textSize; offset = offset + 4) {
-      sprintf(command, "command %2d:     %02x     %02x     %02x     %02x", offset / 4, *(codeStart + offset + 3), *(codeStart + offset + 2), *(codeStart + offset + 1), *(codeStart + offset));
-      ESP_LOGI(TAG, "%s", command);
+   
+   printf("\nmemory dump:\n\n");
+   printf("     byte3  byte2  byte1  byte0\n");
+   for (size_t commandIndex = 0; commandIndex < commandCount; commandIndex++) {
+      uint8_t *firstByteOfCommand = firstByteOfFirstCommand + (commandIndex * ULP_PROGRAM_COMMAND_SIZE_IN_BYTES);
+      sprintf(command, "%2d:     %02x     %02x     %02x     %02x", commandIndex, *(firstByteOfCommand + 3), *(firstByteOfCommand + 2), *(firstByteOfCommand + 1), *(firstByteOfCommand));
+      printf("%s\n", command);
    }
+   printf("\n");
 }
 
-static void printRtcSlowMemory(size_t commandsToPrint) {
-   char command[50];
+static void printUlpProgram(const uint8_t *programStart) {
+   struct UlpBinary *ulpBinary = (struct UlpBinary*)programStart;
+   printf("magic      = %d\n", ulpBinary->magic);
+   printf("textOffset = %d\n", ulpBinary->textOffset);
+   printf("textSize   = %d\n", ulpBinary->textSize);
+   printf("dataSize   = %d\n", ulpBinary->dataSize);
+   printf("bssSize    = %d\n", ulpBinary->bssSize);
+   
+   printCommands(programStart + ulpBinary->textOffset, ulpBinary->textSize);
+}
 
-   ESP_LOGI(TAG, "     byte3  byte2  byte1  byte0");
-   for (size_t commandIndex = 0; commandIndex < commandsToPrint; commandIndex++) {
-      uint8_t *firstByteOfCommand = (uint8_t*)RTC_SLOW_MEM + commandIndex * ULP_PROGRAM_COMMAND_SIZE_IN_BYTES;
-      sprintf(command, "%2d:     %02x     %02x     %02x     %02x", commandIndex, *(firstByteOfCommand + 3), *(firstByteOfCommand + 2), *(firstByteOfCommand + 1), *(firstByteOfCommand));
-      ESP_LOGI(TAG, "%s", command);
+static void printRtcSlowMemory() {
+   size_t commandCount = nextCommandIndex;
+
+   if (commandCount == 0) {
+      printf("No commands entered -> list is empty.\n");
+      return;
+   } 
+
+   if (userEnteredNewCommands) {
+      printf("Please run your program first!\n");
+      return;
    }
+   
+   printCommands((uint8_t*)RTC_SLOW_MEM, commandCount);
 }
 
 static bool regexMatches(const char *text, const char *pattern) {
@@ -233,7 +254,7 @@ static bool regexMatches(const char *text, const char *pattern) {
    char strictMatchingPattern[strlen(pattern) + 2];
    sprintf(strictMatchingPattern, "^%s$", pattern);
    if(regcomp(&regex, strictMatchingPattern, REG_EXTENDED) != 0) {
-      printf("ERROR: failed to compile regex pattern \"%s\"\n", pattern);
+      printf("ERROR: Failed to compile regex pattern \"%s\".\n", pattern);
    } else {
       int result = regexec(&regex, text, 0, NULL, 0);
       regfree(&regex);
@@ -260,7 +281,8 @@ static void createVariable(const char *command) {
 
       size_t commandIndex = nextCommandIndex++;
       setBytesInUlpProgram(commandIndex, &commandBytes);
-      printf("[%u]: variable (value = %d)\n", commandIndex, value);
+      printf("%u: variable (value = %d)\n", commandIndex, value);
+      userEnteredNewCommands = true; 
    }
 }
 
@@ -275,21 +297,30 @@ static void createCommand(const char *command) {
       } else {
          size_t commandIndex = nextCommandIndex++;
          setBytesInUlpProgram(commandIndex, &(result.commandBytes));
-         printf("[%u]: \"%s\"\n", commandIndex, command);
+         printf("%u: \"%s\"\n", commandIndex, command);
+         userEnteredNewCommands = true;
       }
    }
 }
 
-static void runProgram(const char *command) {
+static bool runProgram(const char *command) {
+   bool executedProgram = false;
    char copyOfCommand[strlen(command)];
    strcpy(copyOfCommand, command);
    strtok((char*)copyOfCommand, " ");
    size_t indexOfFirstCommand = atoi(strtok(NULL, " "));
+
    if(indexOfFirstCommand >= nextCommandIndex) {
-      printf("ERROR: maximum allowed command index to start from is %d.\n", nextCommandIndex - 1);
+      if (nextCommandIndex == 0) {
+         printf("ERROR: You need to enter at least one command before calling \"run\".\n");
+      } else {
+         printf("ERROR: Maximum allowed command index to start from is %d.\n", nextCommandIndex - 1);
+      }
    } else {
       appendHaltCommandsToUlpProgram(ulpProgram);
       loadUlpProgram(ulpProgram);
       startUlpProgram(indexOfFirstCommand);
+      executedProgram = true;
    }
+   return executedProgram;
 }
